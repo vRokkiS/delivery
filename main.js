b0eaf69fd753cc9114503f4de2728a465e8d02a1
vRokkiS <56517492+vRokkiS@users.noreply.github.com>
2026-03-17 19:55:15 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -766,7 +766,7 @@ function createDelivery() {
     let item1;
     if ((document.querySelectorAll('.cartData__item').length) >= 1) {
         let cartRow = document.querySelector('.cartData__item:nth-child(1)');
-        item1 = `;document.querySelector('#field1657').value = `${cartRow.querySelector(`.input-name`).value}`;
+        item1 = `;document.querySelector('#field1657').value = "${cartRow.querySelector('.input-name').value}";
         document.querySelector('#field1658').value = 1;
         document.querySelector('#field1659').value = "${cartRow.querySelector('.input-cost').value}";
         document.querySelector('#field1660').value = 1;
@@ -778,7 +778,7 @@ function createDelivery() {
     let item2;
     if ((document.querySelectorAll('.cartData__item').length) >= 2) {
         let cartRow = document.querySelector('.cartData__item:nth-child(2)');
-        item2 = `;document.querySelector('#field1664').value = `${cartRow.querySelector(`.input-name`).value}`;
+        item2 = `;document.querySelector('#field1664').value = "${cartRow.querySelector('.input-name').value}";
         document.querySelector('#field1665').value = 1;
         document.querySelector('#field1666').value = "${cartRow.querySelector('.input-cost').value}";
         document.querySelector('#field1667').value = 1;
@@ -790,7 +790,7 @@ function createDelivery() {
     let item3;
     if ((document.querySelectorAll('.cartData__item').length) >= 3) {
         let cartRow = document.querySelector('.cartData__item:nth-child(3)');
-        item3 = `;document.querySelector('#field1671').value = `${cartRow.querySelector(`.input-name`).value}`;
+        item3 = `;document.querySelector('#field1671').value = "${cartRow.querySelector('.input-name').value}";
         document.querySelector('#field1672').value = 1;
         document.querySelector('#field1673').value = "${cartRow.querySelector('.input-cost').value}";
         document.querySelector('#field1674').value = 1;
@@ -802,7 +802,7 @@ function createDelivery() {
     let item4;
     if ((document.querySelectorAll('.cartData__item').length) >= 4) {
         let cartRow = document.querySelector('.cartData__item:nth-child(4)');
-        item4 = `;document.querySelector('#field1678').value = `${cartRow.querySelector(`.input-name`).value}`;
+        item4 = `;document.querySelector('#field1678').value = "${cartRow.querySelector('.input-name').value}";
         document.querySelector('#field1679').value = 1;
         document.querySelector('#field1680').value = "${cartRow.querySelector('.input-cost').value}";
         document.querySelector('#field1681').value = 1;
@@ -814,7 +814,7 @@ function createDelivery() {
     let item5;
     if ((document.querySelectorAll('.cartData__item').length) >= 5) {
         let cartRow = document.querySelector('.cartData__item:nth-child(5)');
-        item5 = `;document.querySelector('#field1685').value = `${cartRow.querySelector(`.input-name`).value}`;
+        item5 = `;document.querySelector('#field1685').value = "${cartRow.querySelector('.input-name').value}";
         document.querySelector('#field1686').value = 1;
         document.querySelector('#field1687').value = "${cartRow.querySelector('.input-cost').value}";
         document.querySelector('#field1688').value = 1;
